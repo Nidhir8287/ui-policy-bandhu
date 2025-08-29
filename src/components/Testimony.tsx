6e4aad347fee529dcf2ec6ee42e8260256d26156
pnkrkr21 <pankjkr2014@gmail.com>
2025-08-29 20:31:53 +0530

    changed profile photo links

--- src/components/Testimony.tsx
+++ src/components/Testimony.tsx
@@ -4,36 +4,36 @@ const ALL_PEOPLE = [
   {
     name: 'Pankaj Kumar',
     wrote: `I always struggled to understand the fine print of my health insurance plan, but with PolicyBandhu, everything is so clear and easy to grasp. It's a lifesaver!`,
-    picture: 'https://media.licdn.com/dms/image/v2/D4D03AQEw_q_h29ku0g/profile-displayphoto-shrink_400_400/B4DZWKWKXbGkAg-/0/1741782820811?e=1755734400&v=beta&t=crxzEkWWce3s8RtzDEM3fG5zJu1_6TE0VmK_k13ZTIo'
+    picture: 'https://media.licdn.com/dms/image/v2/D4D03AQEw_q_h29ku0g/profile-displayphoto-shrink_400_400/B4DZWKWKXbGkAg-/0/1741782820811?e=1759363200&v=beta&t=KLbljh_2ml8MTYZ2UVv3Rtkiq5DWcRxH12rJygh2ePc'
   },
   {
     name: 'Pradumn Kumar',
     wrote: `I wish I had found PolicyBandhu earlier! It made navigating my health insurance so simple. No more confusion or uncertainty`,
-    picture: `https://media.licdn.com/dms/image/v2/D5603AQFuUlqld5QPTA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1676384620234?e=1755734400&v=beta&t=dHsYu_YCZG8B-8a8GJJ58aL_6L7Lz3PUZNmvefNvW4Q`
+    picture: `https://media.licdn.com/dms/image/v2/D5603AQFuUlqld5QPTA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1676384620234?e=1759363200&v=beta&t=NILTTm2BgUZjEadg8q4zvehHM5mNDxJOpW9TlBZGufY`
   },
   {
     name: 'Rashmi Das',
     wrote: `I’ve been confused for months about my health insurance. PolicyBandhu broke it down in such simple terms. Now I can make informed decisions!`,
-    picture: 'https://media.licdn.com/dms/image/v2/D5603AQF3VqFZyz2s3Q/profile-displayphoto-shrink_400_400/B56Zc.Enp4HUAg-/0/1749093088712?e=1755734400&v=beta&t=MZ4jYCWOPNhUE33EnrRVeEWctU_57NcARkaI8No2Twk'
+    picture: 'https://media.licdn.com/dms/image/v2/D5603AQF3VqFZyz2s3Q/profile-displayphoto-shrink_400_400/B56Zc.Enp4HUAg-/0/1749093088712?e=1759363200&v=beta&t=CuJ52zFOOBYTHpXM-5Gce91rpz4xN0iBMQOQV_sggsI'
   },
   {
     name: 'Paras Ahi',
     wrote: `Great tool! I was able to ask questions about my policy and get instant, easy-to-understand answers. Will definitely use it again.`,
-    picture: 'https://media.licdn.com/dms/image/v2/C4D03AQFoO8JKhBUuqg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1654098183644?e=1755734400&v=beta&t=cKLf_HPRElPRX0giQUvnp03KP0-RW9vVDwg0A0gQfuY',
+    picture: 'https://media.licdn.com/dms/image/v2/C4D03AQFoO8JKhBUuqg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1654098183644?e=1759363200&v=beta&t=H_fivw68fwOJpKjp1iEe1ox-8qiKRenJ_g6qV577qL4',
   },
   {
     name: 'Srishti Srivastav',
     wrote: 'I always found insurance jargon overwhelming. PolicyBandhu made it easy for me to comprehend and gave me confidence to choose the right plan.',
-    picture: 'https://media.licdn.com/dms/image/v2/D4D03AQFPZQ_hDIwJSA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1732553760781?e=1755734400&v=beta&t=Qbi7XT_g-5zvHyvV6RM8nyqtE1_Jj3iEYY79dV5maaY',
+    picture: 'https://media.licdn.com/dms/image/v2/D4D03AQFPZQ_hDIwJSA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1732553760781?e=1759363200&v=beta&t=moXxgNJ63nKY1YR1evotMzGM6lRyuJ4l28ZWIH8Puj4',
   },
   {
     name: 'Tanishq Saini',
     wrote: 'I had no idea what my health insurance policy actually covered. PolicyBandhu’s clear explanations turned all that around for me.',
-    picture: 'https://media.licdn.com/dms/image/v2/C4D03AQHrzTku6I-yrQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1640623159014?e=1755734400&v=beta&t=8vHNEtzM7LNxOi0YQb3k_tdqGrrLiF6hL3394lSj60g',
+    picture: 'https://media.licdn.com/dms/image/v2/C4D03AQHrzTku6I-yrQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1640623159014?e=1759363200&v=beta&t=e0fzt18N8JV7E4wGzDfT3235C1YSFWe3qp9lkd-23Yg',
   },
   {
     name: 'Vimmi Priyadarshini',
-    picture: 'https://media.licdn.com/dms/image/v2/C4E03AQFIDwqYR-xRyA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1654815774394?e=1755734400&v=beta&t=zE6q6Pos0BenaKrILO0bhGb8QNF0KFV3dRXDboaHfiQ',
+    picture: 'https://media.licdn.com/dms/image/v2/C4E03AQFIDwqYR-xRyA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1654815774394?e=1759363200&v=beta&t=FFMYm6aG-N2ssTjR1EwbYhm5LCAc_-XTVz_ZeOm4FcY',
     wrote: `PolicyBandhu helped me understand all the complex clauses in my policy. I now feel secure knowing what I'm covered for`
   }
 ];
